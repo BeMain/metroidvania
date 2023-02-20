107ac1c34f4c63df1cc108611df4953cf7ca4434
BeMain <bemain.dev@gmail.com>
2023-02-20 16:46:45 +0100

    Initial commit

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export_category("Movement")
+@export var WALK_SPEED: int = 300
+@export var JUMP_SPEED: int = 500
+
+# Get the gravity from the project settings so you can sync with rigid body nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	velocity.y += delta * gravity
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -JUMP_SPEED
+	
+	var direction = Input.get_axis("left", "right")
+	velocity.x = direction * WALK_SPEED
+	
+	move_and_slide()

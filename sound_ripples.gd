19434992d97275b66f7ac362fb9642b3158ee3f9
BeMain <bemain.dev@gmail.com>
2023-02-23 21:14:28 +0100

    Add support for multiple simultaneous ripples

--- sound_ripples.gd
+++ sound_ripples.gd
@@ -3,14 +3,16 @@ extends Node
 const sound_ripple_canvas_path: NodePath = ^"/root/World/SoundRippleCanvas"
 const SoundRipple: Resource = preload("res://sound_ripple.tscn")
 
-@onready var sound_ripple_canvas: CanvasLayer = get_node(sound_ripple_canvas_path)
+@onready var sound_ripple_canvas = get_node(sound_ripple_canvas_path)
 
 
 func add_ripple(center: Vector2 = Vector2(0.5, 0.5), force: float = 0.035, thickness: float = 0.0):
 	var ripple: ColorRect = SoundRipple.instantiate()
-	ripple.material.set_shader_parameter("force", force)
-	ripple.material.set_shader_parameter("thickness", thickness)
-	ripple.material.set_shader_parameter("center", center)
+	var material = ripple.material.duplicate(true)
+	material.set_shader_parameter("force", force)
+	material.set_shader_parameter("thickness", thickness)
+	material.set_shader_parameter("center", center)
+	ripple.material = material
 	sound_ripple_canvas.add_child(ripple)
 	ripple.get_node("AnimationPlayer").play("Pulse")
 

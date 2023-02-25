cc43de8f6ce5f81d63e3cf37df414b700576d47f
BeMain <bemain.dev@gmail.com>
2023-02-25 10:58:44 +0100

    Make add_ripple() parameters independent of the screen size

--- player.gd
+++ player.gd
@@ -9,13 +9,18 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	# Gravity
 	velocity.y += delta * gravity
 	
+	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_SPEED
+	
+	# DEBUG: Ripples
 	if Input.is_action_just_pressed("ui_accept"):
 		SoundRipples.add_ripple(position)
 	
+	# Left/right movement
 	var direction = Input.get_axis("left", "right")
 	velocity.x = direction * WALK_SPEED
 	
--- sound_ripples.gd
+++ sound_ripples.gd
@@ -5,26 +5,35 @@ const SoundRipple: Resource = preload("res://sound_ripple.tscn")
 
 @onready var sound_ripple_canvas = get_node(sound_ripple_canvas_path)
 
-
+## Add a sound ripple at a given [position].
+##
+## The ripple if visible for [life_time] seconds.
+## [propagation_speed] is the size of the ripple (in pixels) after 1 second. 
+## The ripple's force is set to [initial_force] and decreases to 0 during the lifetime of the ripple if [decrease_force] is true.
+## The ripple's thickness (in pixels) is set to [initial_thickness] and decreases to 0 during the lifetime of the ripple if [decrease_thickness] is true.
 func add_ripple(position: Vector2 = Vector2.ZERO,
 				life_time: float = 1.0,
-				propagation_speed: float = 1.0,
+				propagation_speed: float = 512.0,
 				initial_force: float = 0.01, 
 				decrease_force: bool = true, 
-				initial_thickness: float = 0.05, 
+				initial_thickness: float = 32.0, 
 				decrease_thickness: bool = false):
+	var viewport_size = get_viewport_rect().size
+	
 	var ripple: ColorRect = SoundRipple.instantiate()
 	var material = ripple.material.duplicate(true)
+	# Set initial values
 	material.set_shader_parameter("size", 0.0)
 	material.set_shader_parameter("force", initial_force)
-	material.set_shader_parameter("thickness", initial_thickness)
-	material.set_shader_parameter("center", position / get_viewport_rect().size)
+	material.set_shader_parameter("thickness", initial_thickness / viewport_size.y)
+	material.set_shader_parameter("center", position / viewport_size)
+	
 	ripple.material = material
 	sound_ripple_canvas.add_child(ripple)
 	
 	# Animate the ripple
 	var tween = get_tree().create_tween()
-	tween.tween_property(ripple, "material:shader_parameter/size", propagation_speed * life_time, life_time)
+	tween.tween_property(ripple, "material:shader_parameter/size", life_time * propagation_speed / viewport_size.y, life_time)
 	if decrease_force:
 		tween.parallel().tween_property(ripple, "material:shader_parameter/force", 0.0, life_time)
 	if decrease_thickness:

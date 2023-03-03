356a86c00def027bba32230a7b939ed0f4c43e35
BeMain <bemain.dev@gmail.com>
2023-03-03 15:35:37 +0100

    Delete old player character

--- player.gd
+++ player.gd
@@ -3,21 +3,9 @@ extends CharacterBody2D
 @export_category("Movement")
 @export var WALK_SPEED: int = 250
 @export var JUMP_SPEED: int = 500
-@export var WALK_ANGLE: int = 45
-var timer = Timer.new()
-var is_on_floor_prev
-var jump_ready
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _ready():
-	timer.connect("timeout", timeout)
-	add_child(timer)
-	timer.set_one_shot(true)
-
-	jump_ready = true
-	is_on_floor_prev = false
 
 func get_input():
 	# Handle jump
@@ -29,18 +17,8 @@ func get_input():
 		SoundRipples.add_ripple(position)
 	
 	# Handle left/right movement
-	#var direction = Input.get_axis("left", "right")
-	#velocity.x = direction * WALK_SPEED
-	
-	
-	var input = Input.get_axis("left", "right")
-	if is_on_floor() and jump_ready and input != 0:
-		var dir = input / abs(input)
-		velocity = Vector2(WALK_SPEED * input, 0).rotated(-dir * deg_to_rad(WALK_ANGLE))
-		jump_ready = false
-
-	if is_on_floor() == true and is_on_floor_prev == false:
-		hit_ground()
+	var direction = Input.get_axis("left", "right")
+	velocity.x = direction * WALK_SPEED
 
 
 func _physics_process(delta):
@@ -49,14 +27,4 @@ func _physics_process(delta):
 	
 	get_input()
 	
-	is_on_floor_prev = is_on_floor()
-
 	move_and_slide()
-
-func timeout():
-	velocity.x = 0
-	jump_ready = true
-	
-
-func hit_ground():
-	timer.start(0.15)

cb4fb183e16fc487a0308958d96369807367f349
BeMain <bemain.dev@gmail.com>
2023-03-04 17:16:45 +0100

    Add running and sneaking

--- player.gd
+++ player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 @export_category("Movement")
+@export var ACCELERATION: int = 50
 @export var WALK_SPEED: int = 250
+@export var SNEAK_MOD: float = 0.6
+@export var RUN_MOD: float = 1.5
+
 @export var JUMP_SPEED: int = 500
+
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,7 +23,15 @@ func get_input():
 	
 	# Handle left/right movement
 	var direction = Input.get_axis("left", "right")
-	velocity.x = direction * WALK_SPEED
+	var target_speed = direction * WALK_SPEED
+	
+	if Input.is_action_pressed("run"):
+		target_speed *= RUN_MOD
+	
+	if Input.is_action_pressed("sneak"):
+		target_speed *= SNEAK_MOD
+	
+	velocity.x = velocity.move_toward(Vector2(target_speed, 0), ACCELERATION).x
 
 
 func _physics_process(delta):

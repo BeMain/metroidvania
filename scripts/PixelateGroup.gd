17b4be07332f961caf8c8d8dbdfa931a4753f7df
BeMain <bemain.dev@gmail.com>
2024-03-25 13:18:52 +0100

    feat(rendering): export pixel size from `PixelateGroup` object

--- scripts/PixelateGroup.gd
+++ scripts/PixelateGroup.gd
@@ -1,6 +1,17 @@
 @tool
 extends CanvasGroup
 
+## The size of the pixels
+@export var pixel_size: int = 4 : set = set_pixel_size
+
+func set_pixel_size(value: int):
+	material.set_shader_parameter("pixel_size", value)
+	pixel_size = value
+
+
+func _ready():
+	set_pixel_size(pixel_size)
+
 func _process(_delta):
 	# TODO: Don't call this every frame, just when the zoom changes.
 	_on_zoom_changed()

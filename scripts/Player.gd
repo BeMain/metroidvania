ecec486fe1498ffe8138ae5c0a3520a679819d05
BeMain <bemain.dev@gmail.com>
2024-04-02 18:26:31 +0200

    refactor(player): cleanup code and enforce GDScript conventions

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,175 +2,176 @@ extends CharacterBody2D
 
 @export var body: PlayerBody
 
-@export_category("Movement")
+@export_subgroup("Movement")
 @export var speed: float = 300.0
 @export var jump_velocity: float = 700.0
 @export var walk_animation_speed: float = 10.0
 
-@export_category("Stomp")
-@export var stomp_speed:float = 1000
-@export var stomp_force:float = 5
+@export_subgroup("Stomp")
+@export var stomp_speed: float = 1000
+@export var stomp_force: float = 5
 
-@export_category("Sneak")
-@export var sneak_speed:float = 100
-@export var sneak_animation_speed:float = 7
+@export_subgroup("Sneak")
+@export var sneak_speed: float = 100
+@export var sneak_animation_speed: float = 7
 
-@export_category("Whistle")
-@export var whistle_strength:float = 5
-@export var whistle_animation_speed:float = 10
-@export var whistle_ripple_interval:float = 3
+@export_subgroup("Whistle")
+@export var whistle_strength: float = 5
+@export var whistle_animation_speed: float = 10
+@export var whistle_ripple_interval: float = 3
 
-@export_category("Duck")
-@export var duck_speed:float = 100
-@export var duck_animation_speed:float = 5
+@export_subgroup("Duck")
+@export var duck_speed: float = 100
+@export var duck_animation_speed: float = 5
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 0
 
-var _cycle_timer:float = 0
+var _cycle_timer: float = 0
 @onready var _old_pos = position
 
-enum _states {normal, sneak, duck, stomp, whistle}
-@onready var _state = _states.normal
+enum State {NORMAL, SNEAK, DUCK, STOMP, WHISTLE}
+var state: State : 
+	get: return _state
+@onready var _state = State.NORMAL
 
 func _physics_process(delta):
-	if _state == _states.normal:
-		if not is_on_floor():
-			velocity.y += gravity * delta * 2
-			_cycle_timer = 0
+	match state:
+		State.NORMAL:
+			if not is_on_floor():
+				velocity.y += gravity * delta * 2
+				_cycle_timer = 0
+				
+				if Input.is_action_just_pressed("down"):
+					_state = State.STOMP
+					velocity.y = stomp_speed
+			else:
+				if Input.is_action_just_pressed("jump"):
+					velocity.y = -jump_velocity
+				if Input.is_action_pressed("sneak"):
+					_state = State.SNEAK
+				if Input.is_action_just_pressed("whistle"):
+					_state = State.WHISTLE
+					body.create_pulsation(3*PI/4*body.direction)
+				if Input.is_action_just_pressed("down"):
+					_state = State.DUCK
+
+			# Get the input direction and handle the movement/deceleration.
+			direction = Input.get_axis("left", "right")
+			if direction:
+				velocity.x = direction * speed
+				_cycle_timer += delta
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				_cycle_timer = 0
 			
-			if Input.is_action_just_pressed("down"):
-				_state = _states.stomp
-				velocity.y = stomp_speed
-		else:
-			if Input.is_action_just_pressed("jump"):
-				velocity.y = -jump_velocity
-			if Input.is_action_pressed("sneak"):
-				_state = _states.sneak
-			if Input.is_action_just_pressed("whistle"):
-				_state = _states.whistle
-				body.create_ripple(3*PI/4*body.direction)
-			if Input.is_action_just_pressed("down"):
-				_state = _states.duck
-
-		# Get the input direction and handle the movement/deceleration.
-		direction = Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * speed
-			_cycle_timer += delta
-		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			_cycle_timer = 0
-		
-		move_and_slide()
-		
-		body.move(_old_pos - position)
-		_old_pos = position
-		
-		var _delta_pos = Vector2.UP * sin(_cycle_timer * walk_animation_speed*2) * 5 if is_on_floor() else Vector2.ZERO
-		body.push(_delta_pos - body.center)
-		
-		var front_leg_offset_angle = -_cycle_timer * walk_animation_speed * direction if is_on_floor() else 0
-		var back_leg_offset_angle = -_cycle_timer * walk_animation_speed * direction + PI if is_on_floor() else 0
-		body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle)*30, cos(front_leg_offset_angle)*10 - 10))
-		body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle)*30, cos(back_leg_offset_angle)*10 - 10))
+			move_and_slide()
+			
+			body.move(_old_pos - position)
+			_old_pos = position
+			
+			var _delta_pos = Vector2.UP * sin(_cycle_timer * walk_animation_speed*2) * 5 if is_on_floor() else Vector2.ZERO
+			body.push(_delta_pos - body.center)
+			
+			var front_leg_offset_angle = -_cycle_timer * walk_animation_speed * direction if is_on_floor() else 0
+			var back_leg_offset_angle = -_cycle_timer * walk_animation_speed * direction + PI if is_on_floor() else 0
+			body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle)*30, cos(front_leg_offset_angle)*10 - 10))
+			body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle)*30, cos(back_leg_offset_angle)*10 - 10))
 	
-	if _state == _states.stomp:
-		velocity.y += gravity * delta * 2
-		velocity.x = 0
-		
-		move_and_slide()
-		
-		body.move(_old_pos - position)
-		_old_pos = position
-		
-		body.push(-body.center)
-		
-		if is_on_floor():
-			SoundRipples.add_ripple(position-(body.radius*Vector2.UP),stomp_force)
-			_state = _states.normal
-	
-	if _state == _states.sneak:
-		if not is_on_floor():
+		State.STOMP:
 			velocity.y += gravity * delta * 2
-			_cycle_timer = 0
-		
-		direction = Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * sneak_speed
-			_cycle_timer += delta
-		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			_cycle_timer = 0
-		
-		move_and_slide()
-		
-		body.move(_old_pos - position)
-		_old_pos = position
-		
-		var _delta_pos = Vector2.UP * sin(_cycle_timer * sneak_animation_speed*2) * 3 if is_on_floor() else Vector2.ZERO
-		body.push(_delta_pos - body.center)
-		
-		var front_leg_offset_angle = -_cycle_timer * sneak_animation_speed * direction if is_on_floor() else 0
-		var back_leg_offset_angle = -_cycle_timer * sneak_animation_speed * direction + PI if is_on_floor() else 0
-		body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle)*10, cos(front_leg_offset_angle)*10 - 10))
-		body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle)*10, cos(back_leg_offset_angle)*10 - 10))
-		
-		body.deform(Vector2.UP,10)
-		body.deform(Vector2(1,0.3),-10)
-		body.deform(Vector2(-1,0.3),-10)
-		
-		if not Input.is_action_pressed("sneak"):
-			_state = _states.normal
+			velocity.x = 0
+			
+			move_and_slide()
+			
+			body.move(_old_pos - position)
+			_old_pos = position
+			
+			body.push(-body.center)
+			
+			if is_on_floor():
+				SoundRipples.add_ripple(position-(body.radius*Vector2.UP), stomp_force)
+				_state = State.NORMAL
 	
-	if _state == _states.whistle:
-		_cycle_timer += delta
-		if _cycle_timer > 0.2:
-			_cycle_timer = 0
-			body.create_ripple(3*PI/4*body.direction)
-			SoundRipples.add_ripple(position,0.2)
-		if not Input.is_action_pressed("whistle"):
-			_state = _states.normal
-		body.set_front_leg_offset(Vector2.ZERO)
-		body.set_back_leg_offset(Vector2.ZERO)
-		body.push(-body.center)
-		body.find_child("Mouth").scale = Vector2(0.02,0.03) * (sin(_cycle_timer/0.2*2*PI)*0.1+1)
-		body.spike_deform(Vector2(1*body.direction,0.3),10)
+		State.SNEAK:
+			if not is_on_floor():
+				velocity.y += gravity * delta * 2
+				_cycle_timer = 0
+			
+			direction = Input.get_axis("left", "right")
+			if direction:
+				velocity.x = direction * sneak_speed
+				_cycle_timer += delta
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				_cycle_timer = 0
+			
+			move_and_slide()
+			
+			body.move(_old_pos - position)
+			_old_pos = position
+			
+			var _delta_pos = Vector2.UP * sin(_cycle_timer * sneak_animation_speed*2) * 3 if is_on_floor() else Vector2.ZERO
+			body.push(_delta_pos - body.center)
+			
+			var front_leg_offset_angle = -_cycle_timer * sneak_animation_speed * direction if is_on_floor() else 0
+			var back_leg_offset_angle = -_cycle_timer * sneak_animation_speed * direction + PI if is_on_floor() else 0
+			body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle) * 10, cos(front_leg_offset_angle) * 10 - 10))
+			body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle) * 10, cos(back_leg_offset_angle) * 10 - 10))
+			
+			body.deform(Vector2.UP, 10)
+			body.deform(Vector2(1, 0.3), -10)
+			body.deform(Vector2(-1, 0.3), -10)
+			
+			if not Input.is_action_pressed("sneak"):
+				_state = State.NORMAL
 	
-	if _state == _states.duck:
-		if not is_on_floor():
-			velocity.y += gravity * delta * 2
-			_cycle_timer = 0
-		
-		direction = Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * duck_speed
+		State.WHISTLE:
 			_cycle_timer += delta
-		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			_cycle_timer = 0
-		
-		move_and_slide()
-		
-		body.move(_old_pos - position)
-		_old_pos = position
-		
-		var _delta_pos = Vector2.UP * sin(_cycle_timer * duck_animation_speed*2) * 4 if is_on_floor() else Vector2.ZERO
-		body.push(_delta_pos - body.center)
-		
-		var front_leg_offset_angle = -_cycle_timer * duck_animation_speed * direction if is_on_floor() else 0
-		var back_leg_offset_angle = -_cycle_timer * duck_animation_speed * direction + PI if is_on_floor() else 0
-		body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle)*30, cos(front_leg_offset_angle)*7 - 7))
-		body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle)*30, cos(back_leg_offset_angle)*7 - 7))
-		
-		body.deform(Vector2.UP,-17)
-		body.deform(Vector2.DOWN,-17)
-		body.spike_deform(Vector2(1,0.3),15)
-		body.spike_deform(Vector2(-1,0.3),15)
-		body.move(Vector2.DOWN*10)
-		
-		if not Input.is_action_pressed("down"):
-			_state = _states.normal
+			if _cycle_timer > 0.2:
+				_cycle_timer = 0
+				body.create_pulsation(3*PI/4*body.direction)
+				SoundRipples.add_ripple(position,0.2)
+			if not Input.is_action_pressed("whistle"):
+				_state = State.NORMAL
+			body.set_front_leg_offset(Vector2.ZERO)
+			body.set_back_leg_offset(Vector2.ZERO)
+			body.push(-body.center)
+			body.find_child("Mouth").scale = Vector2(0.02,0.03) * (sin(_cycle_timer/0.2*2*PI)*0.1+1)
+			body.spike_deform(Vector2(1*body.direction,0.3),10)
+	
+		State.DUCK:
+			if not is_on_floor():
+				velocity.y += gravity * delta * 2
+				_cycle_timer = 0
+			
+			direction = Input.get_axis("left", "right")
+			if direction:
+				velocity.x = direction * duck_speed
+				_cycle_timer += delta
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				_cycle_timer = 0
+			
+			move_and_slide()
+			
+			body.move(_old_pos - position)
+			_old_pos = position
+			
+			var _delta_pos = Vector2.UP * sin(_cycle_timer * duck_animation_speed*2) * 4 if is_on_floor() else Vector2.ZERO
+			body.push(_delta_pos - body.center)
+			
+			var front_leg_offset_angle = -_cycle_timer * duck_animation_speed * direction if is_on_floor() else 0
+			var back_leg_offset_angle = -_cycle_timer * duck_animation_speed * direction + PI if is_on_floor() else 0
+			body.set_front_leg_offset(Vector2(sin(front_leg_offset_angle)*30, cos(front_leg_offset_angle)*7 - 7))
+			body.set_back_leg_offset(Vector2(sin(back_leg_offset_angle)*30, cos(back_leg_offset_angle)*7 - 7))
+			
+			body.deform(Vector2.UP,-17)
+			body.deform(Vector2.DOWN,-17)
+			body.spike_deform(Vector2(1,0.3),15)
+			body.spike_deform(Vector2(-1,0.3),15)
+			body.move(Vector2.DOWN*10)
+			
+			if not Input.is_action_pressed("down"):
+				_state = State.NORMAL
 
-func get_state():
-	return _states.keys()[_state]
--- scripts/PlayerBody.gd
+++ scripts/PlayerBody.gd
@@ -1,41 +1,43 @@
 extends Polygon2D
 class_name PlayerBody
 
-@export_category("Shape")
+@export_subgroup("Shape")
+## Radius of the body
 @export var radius: float = 45.0
+## The number of polygon points that the body consists of
 @export var points: int = 10
 @export var width_multiplier: float = 1.0
 
-@export_category("Behaviour")
+@export_subgroup("Behaviour")
 @export var target_force: float = 2.0
 @export var interaction_force: float = 1.0
 @export var dampening: float = 0.3
 @export var push_spread: float = 0.8
 
-@export_category("Body parts")
+@export_subgroup("Body parts")
+@export var tummy_indexes: Vector2 = Vector2(6, 9)
+@export var eye_offset: Vector2 = Vector2(-10, 15)
+@export var ear_offset: Vector2 = Vector2(-10, -2)
+
+@export_subgroup("Pulsations")
+@export var pulsation_strength: float = 5
+@export var pulsation_speed: float = 25
+@export var pulsation_spread: float = 3
+@export var pulsation_width: float = 0.7
+
 @onready var player: CharacterBody2D = owner
-@onready var tummy = $Tummy
-@export var tummy_indexes:Vector2 = Vector2(5,10)
-@onready var eye = $Eye
-@export var eye_offset: Vector2 = Vector2(0, 0)
-@onready var ear = $Ear
-@export var ear_offset: Vector2
-
-@export_category("Ripple")
-@export var ripple_strength:float = 5
-@export var ripple_speed:float = 25
-@export var ripple_spread:float = 3
-@export var ripple_width:float = 0.7
-
+## The position of the center of the player. 
+## Not always equal to player's position. For example, when moving, the player's center moves up and down, but the actual position stays the same.
 @onready var center = position
-@onready var direction:float = 1
+## The direction the player is facing. `1` indicates right, and `-1` indicates left.
+@onready var direction: float = 1
 @onready var _points_distance = 2*PI * radius / points
 
 var _points = []
 var _target_points = []
 var _velocities = []
 var _poly_points = []
-var _ripples = []
+var _body_pulsations = []
 var _eye_indexes := Vector3(0,0,0)
 var _leg_index: int = 0
 var _leg_front_offset := Vector2.ZERO
@@ -46,14 +48,14 @@ var _ear_position: Vector2
 func _ready():
 	# Create points
 	for i in range(points):
-		var angle = 2*PI*i/points
-		var shape_offset = 1 + cos(angle*3)/15
-		var pos = (Vector2(sin(angle)*width_multiplier,-cos(angle)) * radius * shape_offset) + center
+		var angle = 2*PI * i / points
+		var shape_offset = 1 + cos(angle * 3) / 15
+		var pos = (Vector2(sin(angle) * width_multiplier, -cos(angle)) * radius * shape_offset) + center
 		_points.append(pos)
 		_velocities.append(Vector2.ZERO)
 
 	_target_points = _points.duplicate(true)
-	_points[3] += Vector2(10,10)
+	_points[3] += Vector2(10, 10)
 	
 	# Define bodypart locations
 	_eye_indexes = Vector3(points - int(points / 8.0), int(points / 8.0), int(points / 8.0))
@@ -92,61 +94,61 @@ func _physics_process(delta):
 	for i in range(_points.size()):
 		var pos = _points[i]
 		var neighbour = _points[i+1] if i < points-1 else _points[0]
-		var temp_vec = (neighbour - pos)
-		_poly_points.append(pos + (temp_vec/4))
-		_poly_points.append(pos + (3*temp_vec/4))
+		var distance = (neighbour - pos)
+		_poly_points.append(pos + (distance / 4))
+		_poly_points.append(pos + (3 * distance / 4))
 	
-	# Add ripples
+	# Add pulsations
 	var to_remove = []
-	for i in _ripples:
+	for i in _body_pulsations:
 		for x in range(_poly_points.size()):
-			var progress_clamped = clamp(i["progress"],0,points)
+			var progress_clamped = clamp(i["progress"], 0, points)
 			var distance = min(
 				abs(x - (i["start"] + progress_clamped)),
 				abs(x - (i["start"] - progress_clamped)),
-				abs(x - (i["start"] + progress_clamped) - points*2),
-				abs(x - (i["start"] - progress_clamped) - points*2)
+				abs(x - (i["start"] + progress_clamped) - points * 2),
+				abs(x - (i["start"] - progress_clamped) - points * 2)
 			)
-			var strength = clamp(1-(distance/ripple_spread),0,INF) * clamp(1-(i["progress"]-progress_clamped),0,1)
-			_poly_points[x] += cos(distance/ripple_width) * strength * ripple_strength * (_poly_points[x]-center).normalized()
+			var strength = clamp(1 - (distance / pulsation_spread), 0, INF) * clamp(1 - (i["progress"] - progress_clamped), 0, 1)
+			_poly_points[x] += cos(distance/pulsation_width) * strength * pulsation_strength * (_poly_points[x] - center).normalized()
 			if x == 0:
-				_ear_position += cos(distance/ripple_width) * strength * 2 * ripple_strength * (_poly_points[x]-center).normalized()
-		i["progress"] += delta*ripple_speed
+				_ear_position += cos(distance/pulsation_width) * strength * 2 * pulsation_strength * (_poly_points[x] - center).normalized()
+		i["progress"] += delta*pulsation_speed
 		
 		if i["progress"] > (points+1):
 			to_remove.append(i)
 	for i in to_remove:
-		_ripples.remove_at(_ripples.find(i))
+		_body_pulsations.remove_at(_body_pulsations.find(i))
 	
 	# Draw polygon (body + tummy)
-	var poly_points = _poly_points.slice(tummy_indexes[clamp(direction,0,1)]*direction) + _poly_points.slice(0,tummy_indexes[clamp(-direction,0,1)]*direction)
-	var tummy_polygon = _poly_points.slice(tummy_indexes[0]*direction - clamp(direction,0,1),tummy_indexes[1]*direction + clamp(direction*2,-2,1),direction)
-	var tummy_center = (tummy_polygon[0] + tummy_polygon[-1])/2
-	var tummy_radii = [(tummy_polygon[0]-tummy_center),(tummy_polygon[-1]-tummy_center)]
+	var poly_points = _poly_points.slice(tummy_indexes[clamp(direction, 0, 1)] * direction) + _poly_points.slice(0, tummy_indexes[clamp(-direction, 0, 1)] * direction)
+	var tummy_polygon = _poly_points.slice(tummy_indexes[0]*direction - clamp(direction, 0, 1), tummy_indexes[1] * direction + clamp(direction*2, -2, 1), direction)
+	var tummy_center = (tummy_polygon[0] + tummy_polygon[-1]) / 2
+	var tummy_radii = [(tummy_polygon[0] - tummy_center), (tummy_polygon[-1] - tummy_center)]
 	var vec = tummy_radii[0].normalized()
 	var rad = tummy_radii[0].length()
 	tummy_polygon.reverse()
 	for i in range(5):
-		vec = vec.rotated(-PI/6*direction)
-		rad = move_toward(rad,tummy_radii[1].length(),i/6)
+		vec = vec.rotated(-PI/6 * direction)
+		rad = move_toward(rad, tummy_radii[1].length(), i / 6.0)
 		tummy_polygon.append(tummy_center + (vec*rad))
 	polygon = poly_points
-	tummy.polygon = tummy_polygon
+	$Tummy.polygon = tummy_polygon
 	
 	# Move eye
 	if player.direction:
 		direction = player.direction
 		_eye_indexes.z = _eye_indexes[(direction / 2) + 1]
-	eye.position = _points[_eye_indexes.z] - player.velocity.y * Vector2.UP / 70 + eye_offset * Vector2(direction, 1)
+	$Eye.position = _points[_eye_indexes.z] - player.velocity.y * Vector2.UP / 70 + eye_offset * Vector2(direction, 1)
 	
 	# Rotate the eye
 	if player.velocity:
 		var angle = Vector2.UP.angle_to(player.velocity)
 		angle = direction * clamp(abs(angle), PI/4, 3*PI/4) # Limit the rotation
 		angle -= PI/2 # Rotation is counted relative to the positive x-axis (RIGHT), not the positive positive y-axis (UP).
-		eye.rotation = angle
+		$Eye.rotation = angle
 	else:
-		eye.rotation = PI if direction == -1 else 0.0 # Look straight ahead
+		$Eye.rotation = PI if direction == -1 else 0.0 # Look straight ahead
 	
 	# Move legs
 	var pos
@@ -155,24 +157,24 @@ func _physics_process(delta):
 	else:
 		pos = _points[_leg_index]
 	$LegFront.position = pos + _leg_front_offset
-	$LegFront.global_position.y = clamp($LegFront.global_position.y, -INF, player.global_position.y+38)
+	$LegFront.global_position.y = clamp($LegFront.global_position.y, -INF, player.global_position.y + 38) # Why 38??
 	$LegBack.position = pos + _leg_back_offset
-	$LegBack.global_position.y = clamp($LegBack.global_position.y, -INF, player.global_position.y+38)
+	$LegBack.global_position.y = clamp($LegBack.global_position.y, -INF, player.global_position.y + 38)
 	
 	# Move mouth
-	if player.get_state() == "whistle":
+	if player.state == player.State.WHISTLE:
 		$Mouth.visible = true
-		$Mouth.position = eye.position + Vector2(20*direction,35)
+		$Mouth.position = $Eye.position + Vector2(20 * direction, 35)
 	else:
 		$Mouth.visible = false
 	
 	# Move ear
 	ear_offset.x = abs(ear_offset.x) * -direction
 	_ear_position = _ear_position.lerp(to_global(_points[0] + ear_offset), 0.4)
-	ear.set_points([_points[0] - (Vector2.UP * 5),to_local(_ear_position) - (Vector2.UP * 5)])
+	$Ear.set_points([_points[0] - (Vector2.UP * 5),to_local(_ear_position) - (Vector2.UP * 5)])
 
-# Move entire body without deforming
-func move(distance:Vector2):
+## Move entire body without deforming
+func move(distance: Vector2):
 	# Move points
 	for i in range(_points.size()):
 		_points[i] += distance
@@ -184,12 +186,10 @@ func move(distance:Vector2):
 	# Move center
 	center += distance
 	
-# Move entire body while deforming inwards
-func push(distance:Vector2):
+## Move entire body while deforming inwards
+func push(distance: Vector2):
 	# Move points in similar direction to push
-	for i in range(_points.size()):
-		var strength = clamp(distance.normalized().dot((_points[i]-center).normalized()),0,INF)**push_spread
-		_points[i] += distance * strength
+	deform(distance, 1.0, push_spread)
 	
 	# Move all target points
 	for i in range(_target_points.size()):
@@ -198,25 +198,22 @@ func push(distance:Vector2):
 	# Move center
 	center += distance
 
-# Deform body
-func deform(direction:Vector2,strength:float):
+## Deform body
+func deform(direction: Vector2, strength: float, spread: float = push_spread):
 	direction = direction.normalized()
 	for i in range(_points.size()):
-		var local_strength = clamp(direction.dot((_points[i]-center).normalized()),0,INF)**push_spread
+		var local_strength = clamp(direction.dot((_points[i] - center).normalized()), 0, INF) ** spread
 		_points[i] += direction * strength * local_strength
 
-# Deform body sharply
-func spike_deform(direction:Vector2,strength:float):
-	direction = direction.normalized()
-	for i in range(_points.size()):
-		var local_strength = clamp(direction.dot((_points[i]-center).normalized()),0,INF)**20
-		_points[i] += direction * strength * local_strength
+## Deform body sharply
+func spike_deform(direction: Vector2, strength: float):
+	deform(direction, strength, 20)
 
 func set_front_leg_offset(value: Vector2):
-	_leg_front_offset = _leg_front_offset.lerp(value,0.2)
+	_leg_front_offset = _leg_front_offset.lerp(value, 0.2)
 	
 func set_back_leg_offset(value: Vector2):
-	_leg_back_offset = _leg_back_offset.lerp(value,0.2)
+	_leg_back_offset = _leg_back_offset.lerp(value, 0.2)
 
-func create_ripple(angle:float):
-	_ripples.append({"start":int(angle*2*points/(2*PI)),"progress":0})
+func create_pulsation(angle: float):
+	_body_pulsations.append({"start": int(2 * angle * points / (2*PI)), "progress":0})
